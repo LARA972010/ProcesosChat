588bad3f9fa5ce8c00f845c642aea71b90de6b55
laragarciacarnes <lara972010@hotmail.es>
2024-02-23 12:48:11 +0100

    Controlador de clientes. Casi terminado

--- ChatTCP/src/ClienteHandler.java
+++ ChatTCP/src/ClienteHandler.java
@@ -2,86 +2,69 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
-public class ClienteHandler implements Runnable{
-
-    public static  ArrayList<ClienteHandler>clienteHandler=new ArrayList<>();
+public class ClienteHandler implements Runnable {
+    private static ArrayList<ClienteHandler> clienteHandler = new ArrayList<>();
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
-    private String clientUsername;
+    private String NombreCliente;
 
-    public ClienteHandler(Socket socket){
-        try{
-
-            this.socket=socket;
-            this.bufferedWriter=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            this.bufferedReader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            this.clientUsername=bufferedReader.readLine();
+    public ClienteHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.NombreCliente = bufferedReader.readLine();
             clienteHandler.add(this);
-            broadcastMessage("server: "+clientUsername+" has entrado al chat!");
-        }catch (IOException e){
-            closeEverything(socket,bufferedReader,bufferedWriter);
+            DifundirMensaje("server: " + NombreCliente + " ha entrado al chat!");
+        } catch (IOException e) {
+            CerrarTodo();
         }
-
-
-
     }
+
     @Override
     public void run() {
-        String messageFromClient;
-        while (socket.isConnected()){
-            try{
-                messageFromClient=bufferedReader.readLine();
-                broadcastMessage(messageFromClient);
-            }catch (IOException e){
-                closeEverything(socket,bufferedReader,bufferedWriter);
+        String MensajeCliente;
+        try {
+            while ((MensajeCliente = bufferedReader.readLine()) != null) {
+                DifundirMensaje(NombreCliente + ": " + MensajeCliente);
+            }
+        } catch (IOException e) {
+            CerrarTodo();
+        }
+    }
 
+    public static void DifundirMensaje(String MensajeEnviar) {
+        for (ClienteHandler cliente : clienteHandler) {
+            try {
+                cliente.bufferedWriter.write(MensajeEnviar);
+                cliente.bufferedWriter.newLine();
+                cliente.bufferedWriter.flush();
+            } catch (IOException e) {
+                cliente.CerrarTodo();
             }
         }
     }
 
-    public void broadcastMessage(String messageToSend){
-        for(ClienteHandler clienteHandler:clienteHandler){
-            try{
-                if(!clienteHandler.clientUsername.equals(clientUsername)){
-                    clienteHandler.bufferedWriter.write(messageToSend);
-                    clienteHandler.bufferedWriter.newLine();
-                    clienteHandler.bufferedWriter.flush();
-                }
-            }catch (IOException e){
-                closeEverything(socket,bufferedReader,bufferedWriter);
+    public void salidaCliente() {
+        clienteHandler.remove(this);
+        DifundirMensaje("Server " + NombreCliente + " ha salido del chat.");
+    }
 
-            }
-
-            }
-        }
-
-        public void removeClientHandler(){
-            clienteHandler.remove(this);
-            broadcastMessage("Server "+clientUsername+" ha salido del chat.");
-
-        }
-        public void closeEverything(Socket socket,BufferedReader bufferedReader,BufferedWriter bufferedWriter){
-        removeClientHandler();
-        try{
-
-            if(bufferedReader!=null){
-
+    public void CerrarTodo() {
+        salidaCliente();
+        try {
+            if (bufferedReader != null) {
                 bufferedReader.close();
             }
-            if(bufferedWriter!=null){
-
+            if (bufferedWriter != null) {
                 bufferedWriter.close();
             }
-            if(socket !=null){
-
+            if (socket != null && !socket.isClosed()) {
                 socket.close();
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        }
-
     }
-
-
+}
